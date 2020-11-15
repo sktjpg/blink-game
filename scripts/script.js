b5bfbfa0ec3d322236cb320430403f28e256ccac
sktjpg <uo264003@uniovi.es>
2020-11-15 13:04:32 +0100

    no va

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const Scene = require('Scene');
 const Diagnostics = require('Diagnostics');
 
+
 const Patches = require('Patches');
 const directionalLight = Scene.root.find('directionalLight0');
 var counterNumber = Scene.root.find('number');
@@ -8,3 +9,18 @@ var scoreNumber = Patches.getScalarValue('score');
 
 const directionalLightIntensity = directionalLight.intensity;
 counterNumber.text = scoreNumber.toString();
+
+
+Diagnostics.log(Scene.root.find('gameOver').bounds);
+var reset = Patches.getBooleanValue('reset');
+
+Scene.root.find('gameOver').visible = false;
+
+
+Diagnostics.log(Patches.getBooleanValue("reset").lastValue);
+
+if(Patches.getBooleanValue("reset").lastValue) { 
+    counterNumber.text = "0";
+    counterNumber = 0;
+    Patches.setScalarValue("score", 0);
+}
