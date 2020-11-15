63958702dd17037777731e9b67bdd6c9600c426d
sktjpg <uo264003@uniovi.es>
2020-11-15 17:41:07 +0100

    acabe ya

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,14 @@
 const Scene = require('Scene');
 const Diagnostics = require('Diagnostics');
 
-
 const Patches = require('Patches');
-const directionalLight = Scene.root.find('directionalLight0');
 var counterNumber = Scene.root.find('number');
 var scoreNumber = Patches.getScalarValue('score');
-
-const directionalLightIntensity = directionalLight.intensity;
+var reset = Patches.getBooleanValue('reset').lastValue;
 counterNumber.text = scoreNumber.toString();
 
-
-Diagnostics.log(Scene.root.find('gameOver').bounds);
-var reset = Patches.getBooleanValue('reset');
+Diagnostics.log(reset.toString())
 
 Scene.root.find('gameOver').visible = false;
-
-
-Diagnostics.log(Patches.getBooleanValue("reset").lastValue);
-
-if(Patches.getBooleanValue("reset").lastValue) { 
-    counterNumber.text = "0";
-    counterNumber = 0;
-    Patches.setScalarValue("score", 0);
-}
+ 
+Diagnostics.log(reset); 
